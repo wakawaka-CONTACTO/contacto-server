57566cacf63d7e790644a07bb0a99a2038fed55e
rlarlgnszx <rlarlgnszx@naver.com>
2024-11-08 16:11:55 +0900

    [feat] config 설정 구현 - #11

---
+++ chat-service/src/main/java/org/kiru/chat/config/argumentresolve/UserIdArgumentResolver.java
@@ -0,0 +1,36 @@
+package org.kiru.chat.config.argumentresolve;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isParamHasUserIdAnnotation = parameter.hasParameterAnnotation(UserId.class);
+        boolean isParamLongType = Long.class.equals(parameter.getParameterType());
+        return isParamHasUserIdAnnotation && isParamLongType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        String userIdHeader = webRequest.getHeader("X-User-Id");
+        if (userIdHeader != null) {
+            try {
+                return Long.parseLong(userIdHeader);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Invalid user ID format in X-User-Id header");
+            }
+        } else {
+            throw new IllegalArgumentException("X-User-Id header is missing");
+        }
+    }
+}
