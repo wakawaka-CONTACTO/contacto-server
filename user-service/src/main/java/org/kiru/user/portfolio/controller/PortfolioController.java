15c4aa57124f9f73a2cc143ea4ede5f072e70b7e
rlarlgnszx <rlarlgnszx@naver.com>
2024-11-08 15:52:19 +0900

    [feat] 포트폴리오 패키지 구현 - #5

---
+++ user-service/src/main/java/org/kiru/user/portfolio/controller/PortfolioController.java
@@ -0,0 +1,35 @@
+package org.kiru.user.portfolio.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.kiru.core.user.domain.User;
+import org.kiru.user.auth.argumentresolve.UserId;
+import org.kiru.user.portfolio.dto.res.UserPortfolioResDto;
+import org.kiru.user.portfolio.service.PortfolioService;
+import org.kiru.user.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users/portfolios")
+@RequiredArgsConstructor
+public class PortfolioController {
+
+    private final PortfolioService portfolioService;
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserPortfolioResDto>> getPortfolios(@UserId Long userId) {
+        List<UserPortfolioResDto> portfolios = portfolioService.getUserPortfolios(userId);
+        return ResponseEntity.ok(portfolios);
+    }
+
+    @GetMapping("/{portfolioUserId}")
+    public ResponseEntity<User> getOtherPortfolio(@PathVariable Long portfolioUserId) {
+        User user = userService.getUserFromIdToMainPage(portfolioUserId);
+        return ResponseEntity.ok(user);
+    }
+}
