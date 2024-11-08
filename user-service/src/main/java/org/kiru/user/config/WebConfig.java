5bd967da4ffc4d99a6f46d20a1204497fa269bf0
rlarlgnszx <rlarlgnszx@naver.com>
2024-11-08 15:42:12 +0900

    [feat] user service conf 설정 - #3

---
+++ user-service/src/main/java/org/kiru/user/config/WebConfig.java
@@ -0,0 +1,19 @@
+package org.kiru.user.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.kiru.user.auth.argumentresolve.UserIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UserIdArgumentResolver userIdArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
+    }
+}
