eac5f7aae7004d29a91174b0e2a21aa6b9aa8296
rlarlgnszx <rlarlgnszx@naver.com>
2024-12-07 22:08:11 +0900

    [del] 불필요 줄 - #48

--- chat-service/src/main/java/org/kiru/chat/application/port/in/GetChatRoomUseCase.java
+++ chat-service/src/main/java/org/kiru/chat/application/port/in/GetChatRoomUseCase.java
@@ -2,13 +2,9 @@ package org.kiru.chat.application.port.in;
 
 import java.util.List;
 import org.kiru.core.chat.chatroom.domain.ChatRoom;
-import org.kiru.core.chat.message.domain.Message;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 
 public interface GetChatRoomUseCase {
     ChatRoom findRoomById(Long roomId, Long userId, Boolean isUserAdmin);
     List<ChatRoom> findRoomsByUserId(Long userId);
-
     ChatRoom getOrCreateRoomUseCase(Long userId, Long adminId);
 }
