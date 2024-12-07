66a46ade524ad2c6e0f4fe5bdfbac3b1a25ab6c1
rlarlgnszx <rlarlgnszx@naver.com>
2024-12-07 22:11:17 +0900

    [feat] #48

--- gateway-service/src/main/resources/static/js/config.js
+++ gateway-service/src/main/resources/static/js/config.js
@@ -1,3 +1,7 @@
 const CONFIG = {
-    API_BASE_URL: 'https://api.contacto.p-e.kr'
+    API_BASE_URL: 'https://api.contacto.p-e.kr',
+    API_CHAT_BASE_URL: 'http://chat.contacto-chat.p-e.kr:8081'
+
+    // API_BASE_URL: 'http://localhost:8080',
+    // API_CHAT_BASE_URL: 'http://localhost:8081'
 };
