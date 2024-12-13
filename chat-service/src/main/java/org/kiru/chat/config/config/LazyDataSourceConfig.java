904894478addd6e61a426a66a2a0482e2e2926c0
rlarlgnszx <rlarlgnszx@naver.com>
2024-12-13 23:56:16 +0900

    [chore] lazyDatasource 설정 - #60

--- chat-service/src/main/java/org/kiru/chat/config/config/LazyDataSourceConfig.java
+++ chat-service/src/main/java/org/kiru/chat/config/config/LazyDataSourceConfig.java
@@ -2,20 +2,30 @@ package org.kiru.chat.config.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 @Configuration(proxyBeanMethods = false)
+@RequiredArgsConstructor
 public class LazyDataSourceConfig {
+    @Value("${db.connections}")
+    private int connections;
+
     @Bean
     public DataSource lazyDataSource(DataSourceProperties properties) {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(properties.getUrl());
-        dataSource.setUsername(properties.getUsername());
-        dataSource.setPassword(properties.getPassword());
-        dataSource.setDriverClassName(properties.getDriverClassName());
+        HikariDataSource dataSource = properties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .driverClassName(properties.determineDriverClassName())
+                .password(properties.determinePassword())
+                .url(properties.determineUrl())
+                .username(properties.determineUsername())
+                .build();
+        dataSource.setMaximumPoolSize(connections); // Connection pool size 설정
+        dataSource.setPoolName("HikariPool-Chat");
         return new LazyConnectionDataSourceProxy(dataSource);
     }
 }
--- gateway-service/src/main/java/org/kiru/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/kiru/gateway/GatewayServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
