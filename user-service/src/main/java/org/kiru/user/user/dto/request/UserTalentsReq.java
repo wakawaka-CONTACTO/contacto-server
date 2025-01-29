0bf15eab6f24fd447d063236f3e4fba0625ecdeb
rlarlgnszx <rlarlgnszx@naver.com>
2025-01-29 20:43:05 +0900

    [refactor] userPortoflio 반환값들 변경 - #75

--- user-service/src/main/java/org/kiru/user/user/dto/request/UserTalentsReq.java
+++ user-service/src/main/java/org/kiru/user/user/dto/request/UserTalentsReq.java
@@ -1,7 +1,9 @@
 package org.kiru.user.user.dto.request;
 
+import lombok.Builder;
 import org.kiru.core.user.talent.domain.Talent.TalentType;
 
+@Builder
 public record UserTalentsReq(
         TalentType talentType
         ) {
--- user-service/src/main/java/org/kiru/user/user/dto/request/UserUpdateDto.java
+++ user-service/src/main/java/org/kiru/user/user/dto/request/UserUpdateDto.java
@@ -2,28 +2,26 @@ package org.kiru.user.user.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import org.kiru.core.user.talent.domain.Talent.TalentType;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserUpdateDto {
     @Pattern(regexp = "^[A-Za-z0-9가-힣]{2,20}$",
             message = "이름은 2-20자의 영문자, 숫자, 한글만 가능합니다")
-    private String username;
-
+    private final String username;
     @Email(message = "올바른 이메일 형식이 아닙니다")
-    private String email;
-    private String description;
-    private String instagramId;
-    private String webUrl;
-    private List<Integer> userPurposes;
-    private List<TalentType> userTalents;
-    private Map<Integer, Object> portfolioImages = new HashMap<>();
+    private final String email;
+    private final String description;
+    private final String instagramId;
+    private final String webUrl;
+    private final List<Integer> userPurposes;
+    private final List<TalentType> userTalents;
+    private final Map<Integer, Object> portfolioImages;
 }
