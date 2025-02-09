c30efc57aea471c1d04dc279fcc639a9a03e1955
rlarlgnszx <rlarlgnszx@naver.com>
2025-02-10 01:22:36 +0900

    [hotfix] gatewayConfig - #dev

--- gateway-service/src/main/resources/static/js/config.js
+++ gateway-service/src/main/resources/static/js/config.js
@@ -1,8 +1,3 @@
-// const CONFIG = {
-//     API_BASE_URL: 'https://api.contacto.p-e.kr',
-//     API_CHAT_BASE_URL: 'https://chat.contacto-chat.p-e.kr'
-// };
-
 const CONFIG = {
     API_BASE_URL: 'http://localhost',
     API_CHAT_BASE_URL: 'http://localhost:8081'
