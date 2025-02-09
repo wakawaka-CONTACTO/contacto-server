05630654b3c6049b2724e2180dc743c96f731847
rlarlgnszx <rlarlgnszx@naver.com>
2025-02-09 19:24:44 +0900

    [feat] admin page수정 - #80

--- gateway-service/src/main/resources/static/js/config.js
+++ gateway-service/src/main/resources/static/js/config.js
@@ -1,4 +1,9 @@
+// const CONFIG = {
+//     API_BASE_URL: 'https://api.contacto.p-e.kr',
+//     API_CHAT_BASE_URL: 'https://chat.contacto-chat.p-e.kr'
+// };
+
 const CONFIG = {
-    API_BASE_URL: 'https://api.contacto.p-e.kr',
-    API_CHAT_BASE_URL: 'https://chat.contacto-chat.p-e.kr'
+    API_BASE_URL: 'http://localhost',
+    API_CHAT_BASE_URL: 'http://localhost:8081'
 };
