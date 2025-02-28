ddee4c7082b3d2ed8a62f8675c788c9ac07ca8e9
yunjuwon <a77265192@gmail.com>
2025-02-28 16:51:19 +0900

    fix : 순서변경

--- chat-service/src/main/java/org/kiru/chat/adapter/out/persistence/ChatRoomRepositoryAdapter.java
+++ chat-service/src/main/java/org/kiru/chat/adapter/out/persistence/ChatRoomRepositoryAdapter.java
@@ -38,7 +38,13 @@ public class ChatRoomRepositoryAdapter implements GetChatRoomQuery, SaveChatRoom
     @Transactional
     public ChatRoom save(ChatRoom chatRoom, Long userId, Long userId2) {
         Optional<UserJoinChatRoom> alreadyRoom = userJoinChatRoomRepository.findAlreadyRoomByUserIds(userId, userId2);
-
+        if (alreadyRoom.isPresent()) {
+            log.info("Chat room already exist for user {} and user {}", userId, userId2);
+            return chatRoomRepository.findById(alreadyRoom.get().getChatRoomId())
+                    .map(ChatRoomJpaEntity::toModel)
+                    .orElseThrow(() -> new EntityNotFoundException(FailureCode.CHATROOM_NOT_FOUND));
+        }
+        log.info("Creating chat room for user {} and user {}", userId, userId2);
         ChatRoomJpaEntity entity = ChatRoomJpaEntity.of(chatRoom);
         ChatRoomJpaEntity chatRoomJpa = chatRoomRepository.save(entity);
         UserJoinChatRoom userFirst = UserJoinChatRoom.builder()
@@ -49,14 +55,6 @@ public class ChatRoomRepositoryAdapter implements GetChatRoomQuery, SaveChatRoom
                 .chatRoomId(chatRoomJpa.getId())
                 .userId(userId2)
                 .build();
-        if (alreadyRoom.isPresent()) {
-            log.info("Chat room already exist for user {} and user {}", userId, userId2);
-            return chatRoomRepository.findById(alreadyRoom.get().getChatRoomId())
-                    .map(ChatRoomJpaEntity::toModel)
-                    .orElseThrow(() -> new EntityNotFoundException(FailureCode.CHATROOM_NOT_FOUND));
-        }
-
-        log.info("Creating chat room for user {} and user {}", userId, userId2);
         userJoinChatRoomRepository.saveAll(List.of(userFirst, userSecond));
         return ChatRoomJpaEntity.toModel(chatRoomJpa);
     }
