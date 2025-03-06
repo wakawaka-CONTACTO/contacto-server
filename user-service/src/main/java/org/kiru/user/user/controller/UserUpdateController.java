e402e199d7d4aa7abd948c1dcfb9d34639d0c478
aristo <dkfud2121@naver.com>
2025-03-06 21:48:22 +0900

    refactor: 불필요한 코드라인 정리

--- user-service/src/main/java/org/kiru/user/user/controller/UserUpdateController.java
+++ user-service/src/main/java/org/kiru/user/user/controller/UserUpdateController.java
@@ -1,15 +1,9 @@
 package org.kiru.user.user.controller;
 
 import jakarta.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.kiru.core.user.user.domain.User;
 import org.kiru.user.auth.argumentresolve.UserId;
-import org.kiru.user.portfolio.dto.req.AddMultipartFileDto;
-//import org.kiru.user.portfolio.dto.req.PortfolioImagesRequest;
 import org.kiru.user.user.dto.request.UserUpdateDto;
 import org.kiru.user.user.dto.request.UserUpdatePwdDto;
 import org.kiru.user.user.dto.response.UpdatePwdResponse;
@@ -24,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value.Int;
 
 @RestController
 @RequiredArgsConstructor
--- user-service/src/main/java/org/kiru/user/user/service/UserService.java
+++ user-service/src/main/java/org/kiru/user/user/service/UserService.java
@@ -39,174 +39,180 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @RequiredArgsConstructor
 public class UserService implements GetUserMainPageUseCase {
-    private final UserQueryWithCache userQueryWithCache;
-    private final UserRepository userRepository;
-    private final UserUpdatePort userUpdateport;
-    private final ChatApiClient chatApiClient;
-    private final GetUserAdditionalInfoQuery getUserAdditionalInfoQuery;
-    private final GetUserPortfoliosQuery getUserPortfoliosQuery;
 
-    @Cacheable(value = "userDetail", key = "#userId", unless = "#result == null")
-    public User getUserFromIdToMainPage(Long userId) {
-        User user = userQueryWithCache.getUser(userId);
-        return getUserDetails(user);
+  private final UserQueryWithCache userQueryWithCache;
+  private final UserRepository userRepository;
+  private final UserUpdatePort userUpdateport;
+  private final ChatApiClient chatApiClient;
+  private final GetUserAdditionalInfoQuery getUserAdditionalInfoQuery;
+  private final GetUserPortfoliosQuery getUserPortfoliosQuery;
+
+  @Cacheable(value = "userDetail", key = "#userId", unless = "#result == null")
+  public User getUserFromIdToMainPage(Long userId) {
+    User user = userQueryWithCache.getUser(userId);
+    return getUserDetails(user);
+  }
+
+  public List<ChatRoom> getUserChatRooms(Long userId, Pageable pageable) {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      CompletableFuture<List<ChatRoom>> chatRoomsFuture = CompletableFuture.supplyAsync(
+          () -> chatApiClient.getUserChatRooms(userId, pageable),
+          executor);
+      CompletableFuture<List<UserPortfolioItem>> allParticipantIdsFuture = chatRoomsFuture.thenApplyAsync(
+          ChatRoom::getAllParticipantIds, executor).thenApplyAsync(
+          getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor);
+      CompletableFuture<Map<Long, UserPortfolioItem>> userPortfolioImgMapFuture = allParticipantIdsFuture.thenApplyAsync(
+          UserPortfolio::getUserIdAndUserPortfolioItemMap, executor);
+      return chatRoomsFuture.thenCombineAsync(userPortfolioImgMapFuture,
+          (chatRooms, userPortfolioImgMap) -> {
+            for (ChatRoom chatRoom : chatRooms) {
+              chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap);
+            }
+            return chatRooms;
+          }, executor).join();
     }
+  }
 
-    public List<ChatRoom> getUserChatRooms(Long userId, Pageable pageable) {
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            CompletableFuture<List<ChatRoom>> chatRoomsFuture = CompletableFuture.supplyAsync(
-                    () -> chatApiClient.getUserChatRooms(userId, pageable),
-                    executor);
-            CompletableFuture<List<UserPortfolioItem>> allParticipantIdsFuture = chatRoomsFuture.thenApplyAsync(
-                    ChatRoom::getAllParticipantIds, executor).thenApplyAsync(
-                    getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor);
-            CompletableFuture<Map<Long, UserPortfolioItem>> userPortfolioImgMapFuture = allParticipantIdsFuture.thenApplyAsync(
-                    UserPortfolio::getUserIdAndUserPortfolioItemMap,executor);
-            return chatRoomsFuture.thenCombineAsync(userPortfolioImgMapFuture, (chatRooms, userPortfolioImgMap) -> {
-                for (ChatRoom chatRoom : chatRooms) {
-                    chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap);
-                }
-                return chatRooms;
-            }, executor).join();
-        }
+  public ChatRoomResponse getChatMessage(Long roomId, Long userId, int page, int size) {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      CompletableFuture<ChatRoom> chatRoomFuture = CompletableFuture.supplyAsync(
+          () -> chatApiClient.getRoom(roomId, userId), executor
+      );
+
+      CompletableFuture<List<UserPortfolioItem>> userPortfolioImgMapFuture = chatRoomFuture.thenApplyAsync(
+          ChatRoom::getParticipantsIds, executor).thenApplyAsync(
+          getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor
+      );
+
+      Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+      CompletableFuture<List<MessageResponse>> messageFuture = CompletableFuture.supplyAsync(() -> {
+        return chatApiClient.getMessages(roomId, userId, false, pageable)
+            .stream()
+            .map(MessageResponse::fromMessage)
+            .toList();
+      }, executor);
+
+      return chatRoomFuture.thenCombineAsync(userPortfolioImgMapFuture,
+          (chatRoom, userPortfolioImgMap) -> {
+            chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap.getFirst());
+            return chatRoom;
+          }, executor).thenCombineAsync(messageFuture, ChatRoomResponse::of, executor).join();
     }
+  }
 
-    public ChatRoomResponse getChatMessage(Long roomId, Long userId, int page, int size){
-        try( var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            CompletableFuture<ChatRoom> chatRoomFuture = CompletableFuture.supplyAsync(
-                () -> chatApiClient.getRoom(roomId, userId), executor
-            );
-
-            CompletableFuture<List<UserPortfolioItem>> userPortfolioImgMapFuture= chatRoomFuture.thenApplyAsync(
-                ChatRoom::getParticipantsIds, executor).thenApplyAsync(
-                    getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor
-            );
-
-            Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
-            CompletableFuture<List<MessageResponse>> messageFuture = CompletableFuture.supplyAsync(() -> {
-                return chatApiClient.getMessages(roomId, userId, false, pageable)
-                    .stream()
-                    .map(MessageResponse::fromMessage)
-                    .toList();
-            }, executor);
-
-            return chatRoomFuture.thenCombineAsync(userPortfolioImgMapFuture, (chatRoom, userPortfolioImgMap) -> {
-                chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap.getFirst());
-                return chatRoom;
-            }, executor).thenCombineAsync(messageFuture, ChatRoomResponse::of, executor).join();
-        }
+  public ChatRoom getUserChatRoom(Long roomId, Long userId) {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      CompletableFuture<ChatRoom> chatRoomFuture = CompletableFuture.supplyAsync(
+          () -> chatApiClient.getRoom(roomId, userId), executor);
+      CompletableFuture<List<UserPortfolioItem>> userPortfolioImgMapFuture = chatRoomFuture.thenApplyAsync(
+          ChatRoom::getParticipantsIds, executor).thenApplyAsync(
+          getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor);
+      return chatRoomFuture.thenCombineAsync(userPortfolioImgMapFuture,
+          (chatRoom, userPortfolioImgMap) -> {
+            chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap.getFirst());
+            return chatRoom;
+          }, executor).join();
     }
+  }
 
-    public ChatRoom getUserChatRoom(Long roomId, Long userId) {
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            CompletableFuture<ChatRoom> chatRoomFuture = CompletableFuture.supplyAsync(
-                    () -> chatApiClient.getRoom(roomId, userId), executor);
-            CompletableFuture<List<UserPortfolioItem>> userPortfolioImgMapFuture = chatRoomFuture.thenApplyAsync(
-                    ChatRoom::getParticipantsIds, executor).thenApplyAsync(
-                    getUserPortfoliosQuery::getUserPortfoliosWithMinSequence, executor);
-            return chatRoomFuture.thenCombineAsync(userPortfolioImgMapFuture, (chatRoom, userPortfolioImgMap) -> {
-                chatRoom.setThumbnailAndRoomTitle(userPortfolioImgMap.getFirst());
-                return chatRoom;
-            }, executor).join();
-        }
+  public User getUserDetails(User user) {
+    Long userId = user.getId();
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      CompletableFuture<List<PurposeType>> purposesFuture = CompletableFuture.supplyAsync(
+          () -> getUserAdditionalInfoQuery.getUserPurposes(userId), executor);
+      CompletableFuture<List<TalentType>> talentsFuture = CompletableFuture.supplyAsync(
+          () -> getUserAdditionalInfoQuery.getUserTalents(userId),
+          executor);
+      CompletableFuture<List<UserPortfolioItem>> portfolioImgsFuture = CompletableFuture.supplyAsync(
+          () -> getUserAdditionalInfoQuery.getUserPortfolioByUserId(userId), executor);
+      return CompletableFuture.allOf(purposesFuture, talentsFuture, portfolioImgsFuture)
+          .thenApplyAsync(v -> {
+            user.userPurposes(purposesFuture.join());
+            user.userTalents(talentsFuture.join());
+            user.userPortfolio(UserPortfolio.of(portfolioImgsFuture.join()));
+            return user;
+          }).join();
     }
+  }
 
-    public User getUserDetails(User user) {
-        Long userId = user.getId();
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            CompletableFuture<List<PurposeType>> purposesFuture = CompletableFuture.supplyAsync(
-                    () -> getUserAdditionalInfoQuery.getUserPurposes(userId), executor);
-            CompletableFuture<List<TalentType>> talentsFuture = CompletableFuture.supplyAsync(
-                    () -> getUserAdditionalInfoQuery.getUserTalents(userId),
-                    executor);
-            CompletableFuture<List<UserPortfolioItem>> portfolioImgsFuture = CompletableFuture.supplyAsync(
-                    () -> getUserAdditionalInfoQuery.getUserPortfolioByUserId(userId), executor);
-            return CompletableFuture.allOf(purposesFuture, talentsFuture, portfolioImgsFuture)
-                    .thenApplyAsync(v -> {
-                        user.userPurposes(purposesFuture.join());
-                        user.userTalents(talentsFuture.join());
-                        user.userPortfolio(UserPortfolio.of(portfolioImgsFuture.join()));
-                        return user;
-                    }).join();
-        }
-    }
+  @Transactional
+  @CachePut(value = "userDetail", key = "#userId", unless = "#result == null")
+  public User updateUser(final Long userId, final UserUpdateDto userUpdateDto) {
+    UserJpaEntity existingEntity = userRepository.findById(userId)
+        .orElseThrow(() -> new EntityNotFoundException(FailureCode.USER_NOT_FOUND));
 
-    @Transactional
-    @CachePut(value = "userDetail", key = "#userId", unless = "#result == null")
-    public User updateUser(final Long userId, final UserUpdateDto userUpdateDto) {
-        UserJpaEntity existingEntity = userRepository.findById(userId)
-            .orElseThrow(() -> new EntityNotFoundException(FailureCode.USER_NOT_FOUND));
-        User updatedData = User.builder()
-            .id(existingEntity.getId())
-            .username(userUpdateDto.getUsername() != null ? userUpdateDto.getUsername() : null)
-            .email(userUpdateDto.getEmail() != null ? userUpdateDto.getEmail() : null)
-            .description(userUpdateDto.getDescription() != null ? userUpdateDto.getDescription() : null)
-            .instagramId(userUpdateDto.getInstagramId() != null ? userUpdateDto.getInstagramId() : null)
-            .webUrl(userUpdateDto.getWebUrl() != null ? userUpdateDto.getWebUrl() : null)
-            .password(null)
-            .loginType(null)
-            .build();
-        existingEntity.updateDetails(updatedData);
+    User updatedData = mapDtoToUser(existingEntity, userUpdateDto);
+    existingEntity.updateDetails(updatedData);
+    User savedUser = UserJpaEntity.toModel(existingEntity);
 
-        UserJpaEntity savedEntity = userRepository.save(existingEntity);
-        User savedUser = UserJpaEntity.toModel(savedEntity);
+    updateAssociatedUserDetails(userId, userUpdateDto, savedUser);
 
-        CompletableFuture<List<PurposeType>> purposesFuture = CompletableFuture.supplyAsync(
-                () -> userUpdateport.updateUserPurposes(userId, userUpdateDto)).thenApplyAsync(purposeTypeList -> {
-            savedUser.userPurposes(purposeTypeList);
-            return purposeTypeList;
+    userQueryWithCache.saveExistUser(savedUser);
+    savedUser.getUserPortfolio().sort();
+    return savedUser;
+  }
+
+  private User mapDtoToUser(UserJpaEntity userJpaEntity, UserUpdateDto userUpdateDto) {
+    User updatedData = User.builder()
+        .id(userJpaEntity.getId())
+        .username(userUpdateDto.getUsername() != null ? userUpdateDto.getUsername() : null)
+        .email(userUpdateDto.getEmail() != null ? userUpdateDto.getEmail() : null)
+        .description(userUpdateDto.getDescription() != null ? userUpdateDto.getDescription() : null)
+        .instagramId(userUpdateDto.getInstagramId() != null ? userUpdateDto.getInstagramId() : null)
+        .webUrl(userUpdateDto.getWebUrl() != null ? userUpdateDto.getWebUrl() : null)
+        .password(null)
+        .loginType(null)
+        .build();
+
+    return updatedData;
+  }
+
+  private void updateAssociatedUserDetails(Long userId, UserUpdateDto userUpdateDto,
+      User savedUser) {
+    CompletableFuture<List<PurposeType>> purposesFuture = CompletableFuture.supplyAsync(
+            () -> userUpdateport.updateUserPurposes(userId, userUpdateDto))
+        .thenApplyAsync(purposeTypeList -> {
+          savedUser.userPurposes(purposeTypeList);
+          return purposeTypeList;
         });
-        CompletableFuture<List<TalentType>> talentsFuture = CompletableFuture.supplyAsync(
-                () -> userUpdateport.updateUserTalents(userId, userUpdateDto)).thenApplyAsync(talentTypeList -> {
-            savedUser.userTalents(talentTypeList);
-            return talentTypeList;
+    CompletableFuture<List<TalentType>> talentsFuture = CompletableFuture.supplyAsync(
+            () -> userUpdateport.updateUserTalents(userId, userUpdateDto))
+        .thenApplyAsync(talentTypeList -> {
+          savedUser.userTalents(talentTypeList);
+          return talentTypeList;
         });
-        CompletableFuture<UserPortfolio> userPortfolioFuture = CompletableFuture.supplyAsync(
-                () -> userUpdateport.updateUserPortfolioImages(userId, userUpdateDto)).thenApplyAsync(userPortfolio -> {
-            savedUser.userPortfolio(userPortfolio);
-            return userPortfolio;
+    CompletableFuture<UserPortfolio> userPortfolioFuture = CompletableFuture.supplyAsync(
+            () -> userUpdateport.updateUserPortfolioImages(userId, userUpdateDto))
+        .thenApplyAsync(userPortfolio -> {
+          savedUser.userPortfolio(userPortfolio);
+          return userPortfolio;
         });
-        CompletableFuture.allOf(purposesFuture, talentsFuture, userPortfolioFuture).join();
-        userQueryWithCache.saveExistUser(savedUser);
-        savedUser.getUserPortfolio().sort();
-        return savedUser;
-    }
+    CompletableFuture.allOf(purposesFuture, talentsFuture, userPortfolioFuture).join();
+  }
 
-    private User updateUserDetails(User user, UserUpdateDto userUpdateDto) {
-        return  User.builder()
-                .id(user.getId())
-                .username(userUpdateDto.getUsername())
-                .webUrl(userUpdateDto.getWebUrl())
-//                .email(userUpdateDto.getEmail())
-//                .password(userUpdateDto.getPassword())
-                .description(userUpdateDto.getDescription())
-                .instagramId(userUpdateDto.getInstagramId())
-                .build();
+  @Transactional
+  public Boolean updateUserPwd(UserUpdatePwdDto userUpdatePwdDto) {
+    Optional<UserJpaEntity> existingUser = userRepository.findByEmail(userUpdatePwdDto.email());
+    if (existingUser.isPresent()) {
+      return userUpdateport.updateUserPwd(existingUser.get(), userUpdatePwdDto);
+    } else {
+      return false;
     }
+  }
 
-    @Transactional
-    public Boolean updateUserPwd(UserUpdatePwdDto userUpdatePwdDto) {
-        Optional<UserJpaEntity> existingUser = userRepository.findByEmail(userUpdatePwdDto.email());
-        if (existingUser.isPresent()) {
-            return userUpdateport.updateUserPwd(existingUser.get(), userUpdatePwdDto);
-        } else {
-            return false;
-        }
-    }
+  @Cacheable(value = "user", key = "#email")
+  public User findExistUserByEmail(String email) {
+    return userRepository.findByEmail(email)
+        .map(UserJpaEntity::toModel)
+        .orElseThrow(() -> new EntityNotFoundException(FailureCode.USER_NOT_FOUND));
+  }
 
-    @Cacheable(value = "user", key = "#email")
-    public User findExistUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .map(UserJpaEntity::toModel)
-                .orElseThrow(() -> new EntityNotFoundException(FailureCode.USER_NOT_FOUND));
-    }
+  @Transactional
+  public void deleteUser(Long userId) {
+    userRepository.deleteById(userId);
+  }
 
-    @Transactional
-    public void deleteUser(Long userId) {
-        userRepository.deleteById(userId);
-    }
-
-    public boolean existsByEmail(String email) {
-        return userRepository.findByEmail(email).isPresent();
-    }
+  public boolean existsByEmail(String email) {
+    return userRepository.findByEmail(email).isPresent();
+  }
 }
