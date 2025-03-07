6d02579b107c9b4f15aa8177eba0991286943786
juwon <dlegh476@tukorea.ac.kr>
2025-03-07 15:28:26 +0900

    Revert "[FIX] emailsend시 Response 객체 생성"

--- user-service/src/main/java/org/kiru/user/auth/mail/controller/MailController.java
+++ user-service/src/main/java/org/kiru/user/auth/mail/controller/MailController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.kiru.user.auth.mail.dto.MailCheckDto;
 import org.kiru.user.auth.mail.dto.MailCheckResponse;
 import org.kiru.user.auth.mail.dto.MailSendDto;
-import org.kiru.user.auth.mail.dto.MailSendResponse;
 import org.kiru.user.auth.mail.service.MailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +20,8 @@ public class MailController {
     private final MailService mailService;
 
     @PostMapping("/emailsend")
-    public ResponseEntity<MailSendResponse> emailCheck(@RequestBody MailSendDto mailSendDTO) throws MessagingException {
-        return ResponseEntity.ok(new MailSendResponse(mailService.sendSimpleMessage(mailSendDTO.getEmail()))); // Response body에 값을 반환
+    public String emailCheck(@RequestBody MailSendDto mailSendDTO) throws MessagingException {
+        return mailService.sendSimpleMessage(mailSendDTO.getEmail()); // Response body에 값을 반환
     }
 
     @PostMapping("/emailcheck")
