7bd02fdbcb11f75296e2e35fef0cf902306bc633
aristo <dkfud2121@naver.com>
2025-03-11 12:42:15 +0900

    [Feat] 이메일 인증 메일 전송 과정 비동기 처리 -#136

--- user-service/src/main/java/org/kiru/user/auth/mail/service/MailService.java
+++ user-service/src/main/java/org/kiru/user/auth/mail/service/MailService.java
@@ -3,6 +3,8 @@ package org.kiru.user.auth.mail.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.kiru.core.exception.ConflictException;
@@ -18,65 +20,69 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class MailService {
-    private final JavaMailSender javaMailSender;
-    private final UserService userService;
-    private final RedisTemplate<String, String> redisTemplateForOne ;
-    private static final String senderEmail = "rlarlgnszx0319@gmail.com";
 
-    // 랜덤으로 6자리 숫자 생성
-    public String createNumber() {
-        Random random = new Random();
-        StringBuilder key = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            key.append(random.nextInt(10));
-        }
-        return key.toString();
+  private final JavaMailSender javaMailSender;
+  private final UserService userService;
+  private final RedisTemplate<String, String> redisTemplateForOne;
+  private static final String senderEmail = "rlarlgnszx0319@gmail.com";
+
+  // 랜덤으로 6자리 숫자 생성
+  public String createNumber() {
+    Random random = new Random();
+    StringBuilder key = new StringBuilder();
+    for (int i = 0; i < 6; i++) {
+      key.append(random.nextInt(10));
     }
+    return key.toString();
+  }
 
-    public MimeMessage createMail(String mail, String number) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        message.setFrom(senderEmail);
-        message.setRecipients(MimeMessage.RecipientType.TO, mail);
-        message.setSubject("이메일 인증");
-        String body = "";
-        body += "<h3>요청하신 인증 번호입니다.</h3>";
-        body += "<h1>" + number + "</h1>";
-        body += "<h3>감사합니다.</h3>";
-        message.setText(body, "UTF-8", "html");
-        return message;
+  public MimeMessage createMail(String mail, String number) throws MessagingException {
+    MimeMessage message = javaMailSender.createMimeMessage();
+    message.setFrom(senderEmail);
+    message.setRecipients(MimeMessage.RecipientType.TO, mail);
+    message.setSubject("이메일 인증");
+    String body = "";
+    body += "<h3>요청하신 인증 번호입니다.</h3>";
+    body += "<h1>" + number + "</h1>";
+    body += "<h3>감사합니다.</h3>";
+    message.setText(body, "UTF-8", "html");
+    return message;
+  }
+
+  private void validateEmailNotExists(String email) {
+    if (userService.existsByEmail(email)) {
+      throw new ConflictException(FailureCode.DUPLICATE_EMAIL);
     }
+  }
 
-    private void validateEmailNotExists(String email) {
-        if (userService.existsByEmail(email)) {
-            throw new ConflictException(FailureCode.DUPLICATE_EMAIL);
-        }
-    }
+  // 메일 발송
+  public void sendSimpleMessage(String sendEmail) throws MessagingException {
+    validateEmailNotExists(sendEmail);
+    String number = createNumber(); // 랜덤 인증번호 생성
+    sendMail(sendEmail, number);
+  }
 
-    // 메일 발송
-    public void sendSimpleMessage(String sendEmail) throws MessagingException {
-        validateEmailNotExists(sendEmail);
-        String number = createNumber(); // 랜덤 인증번호 생성
-        sendMail(sendEmail, number);
-    }
+  private void sendMail(String sendEmail, String number) throws MessagingException {
+    MimeMessage message = createMail(sendEmail, number); // 메일 생성
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      CompletableFuture.runAsync(() -> {
+        javaMailSender.send(message);
+      }, executor);
 
-    private void sendMail(String sendEmail, String number) throws MessagingException{
-        MimeMessage message = createMail(sendEmail, number); // 메일 생성
-
-        try {
-            javaMailSender.send(message); // 메일 발송
+      CompletableFuture.runAsync(() -> {
             redisTemplateForOne.opsForValue().set(sendEmail, number);
-        } catch (MailException e) {
-            log.error(e.getMessage());
-            throw new IllegalArgumentException("메일 발송 중 오류가 발생했습니다.");
-        }
+          }, executor);
+    } catch (MailException e) {
+      log.error(e.getMessage());
+      throw new IllegalArgumentException("메일 발송 중 오류가 발생했습니다.");
     }
+  }
 
-    public Boolean checkMessage(MailCheckDto mailCheckDto) {
-        String savedNumber = redisTemplateForOne.opsForValue().get(mailCheckDto.getEmail());
-        if (savedNumber != null && savedNumber.equals(mailCheckDto.getAuthCode())) {
-            return true;
-        }
-        return false;
+  public Boolean checkMessage(MailCheckDto mailCheckDto) {
+    String savedNumber = redisTemplateForOne.opsForValue().get(mailCheckDto.getEmail());
+    if (savedNumber != null && savedNumber.equals(mailCheckDto.getAuthCode())) {
+      return true;
     }
-
+    return false;
+  }
 }
