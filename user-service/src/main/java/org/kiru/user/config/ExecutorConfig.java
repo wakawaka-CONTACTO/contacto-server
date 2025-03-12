76d3a303d2a26a95dbc00cffbbcb3138309e90c3
aristo <dkfud2121@naver.com>
2025-03-12 16:45:10 +0900

    [Feat] -#139 ExecutorConfig 앞에 EnableAsync 어노테이션 추가

--- user-service/src/main/java/org/kiru/user/config/ExecutorConfig.java
+++ user-service/src/main/java/org/kiru/user/config/ExecutorConfig.java
@@ -4,9 +4,11 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @Configuration
+@EnableAsync
 public class ExecutorConfig {
     @Bean
     public Executor virtualThreadExecutor() {
