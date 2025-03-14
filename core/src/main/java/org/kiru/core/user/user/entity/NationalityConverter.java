4e1be887d90d45fcd2fd60a180b5e4a0143920ae
aristo <dkfud2121@naver.com>
2025-03-14 13:05:26 +0900

    fix [#141] change converter from Jacson converter to springframework converter

--- core/src/main/java/org/kiru/core/user/user/entity/NationalityConverter.java
+++ core/src/main/java/org/kiru/core/user/user/entity/NationalityConverter.java
@@ -1,25 +1,13 @@
 package org.kiru.core.user.user.entity;
 
-
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.type.TypeFactory;
-import com.fasterxml.jackson.databind.util.Converter;
+import org.springframework.core.convert.converter.Converter;
 import org.kiru.core.user.user.domain.Nationality;
+
 
 public class NationalityConverter implements Converter<Nationality, String> {
 
   @Override
   public String convert(Nationality source) {
     return source == null ? null : source.getCode();
-  }
-
-  @Override
-  public JavaType getInputType(TypeFactory typeFactory) {
-    return typeFactory.constructType(Nationality.class);
-  }
-
-  @Override
-  public JavaType getOutputType(TypeFactory typeFactory) {
-    return typeFactory.constructType(String.class);
   }
 }
