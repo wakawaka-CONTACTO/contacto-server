d22c23ff563132943cb1479b0826fca9449be0c8
yunjuwon <a77265192@gmail.com>
2025-03-19 20:00:01 +0900

    feat [#150] Alarm service 만듬

---
+++ alarm-service/src/main/java/org/kiru/alarm_service/AlarmServiceApplication.java
@@ -0,0 +1,19 @@
+package org.kiru.alarm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"org.kiru.core.alarm"})
+public class AlarmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmServiceApplication.class, args);
+	}
+
+}
