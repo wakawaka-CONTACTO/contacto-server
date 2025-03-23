beb3685b6a91c2c8e4c4e48b51ff03ad12a321ff
aristo <dkfud2121@naver.com>
2025-03-24 00:30:28 +0900

    feat: 사진 유형에 따른 이미지 리사이징 로직 구현

--- user-service/src/main/java/org/kiru/user/portfolio/constants/PhotoUsage.java
+++ user-service/src/main/java/org/kiru/user/portfolio/constants/PhotoUsage.java
@@ -1,4 +1,7 @@
 package org.kiru.user.portfolio.constants;
 
 public enum PhotoUsage {
+  CHAT_PROFILE,
+  PORTFOLIO_SCROLL,
+  FULL_SCREEN
 }
--- user-service/src/main/java/org/kiru/user/portfolio/service/PhotoOptimizer.java
+++ user-service/src/main/java/org/kiru/user/portfolio/service/PhotoOptimizer.java
@@ -5,9 +5,32 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import net.coobird.thumbnailator.Thumbnails;
+import org.kiru.user.portfolio.constants.PhotoUsage;
 import org.springframework.web.multipart.MultipartFile;
 
 public class PhotoOptimizer {
+
+  public InputStream optimize(MultipartFile multipartFile, PhotoUsage usage) throws IOException {
+    int width, height;
+    switch (usage) {
+      case CHAT_PROFILE:
+        width = 100;
+        height = 100;
+        break;
+      case PORTFOLIO_SCROLL:
+        width = 198;
+        height = 260;
+        break;
+      case FULL_SCREEN:
+        width = 1080;
+        height = 1920;
+        break;
+      default:
+        throw new IllegalArgumentException("Unknown PhotoUsage: " + usage);
+    }
+    return resize(multipartFile, width, height);
+  }
+
   public InputStream resize(MultipartFile multipartFile, int width, int height) throws IOException {
     try (InputStream originalInputStream = multipartFile.getInputStream();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
