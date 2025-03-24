3139817f8bbf6ee25b9c96f28d124e22e4604388
aristo <dkfud2121@naver.com>
2025-03-24 16:33:05 +0900

    fix: 회원가입 중 포트폴리오 이미지 업로드 실패 - #157

--- user-service/src/main/java/org/kiru/user/external/s3/ImageService.java
+++ user-service/src/main/java/org/kiru/user/external/s3/ImageService.java
@@ -3,7 +3,6 @@ package org.kiru.user.external.s3;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
