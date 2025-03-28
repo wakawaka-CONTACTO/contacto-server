6d1ddb4f218137ec8e6bae8829829099c62e863a
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-03-28 17:52:13 +0900

    Update AlarmServiceApplication.java

--- alarm-service/src/main/java/org/kiru/alarm/AlarmServiceApplication.java
+++ alarm-service/src/main/java/org/kiru/alarm/AlarmServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EntityScan(basePackages = {"org.kiru.core.device"})
 public class AlarmServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AlarmServiceApplication.class, args);
+		SpringApplication.run(AlarmServiceApplication.class, args); 
 	}
 
 }
