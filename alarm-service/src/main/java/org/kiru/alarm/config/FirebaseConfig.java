8ae540cda74a16ccf765b05744f4a56191d2510b
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-03-28 18:48:01 +0900

    Revert "[Fix] CD 진행 시 JSON  파싱 되도록 수정  - #175"

--- alarm-service/src/main/java/org/kiru/alarm/config/FirebaseConfig.java
+++ alarm-service/src/main/java/org/kiru/alarm/config/FirebaseConfig.java
@@ -3,17 +3,14 @@ package org.kiru.alarm.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.gson.GsonBuilder;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @Configuration
 @Slf4j
@@ -23,11 +20,7 @@ public class FirebaseConfig {
     public void init() {
         // 서비스 계정 키 경로
         Resource resource = new ClassPathResource("serviceAccountKey.json");
-        try (InputStream serviceAccount = resource.getInputStream()) {
-            // GsonBuilder를 사용하여 lenient 모드 설정
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            gsonBuilder.setLenient();
-
+        try ( InputStream serviceAccount = resource.getInputStream()){
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
@@ -41,24 +34,5 @@ public class FirebaseConfig {
         } catch (IOException e) {
             log.error("FirebaseApp 초기화 실패", e);
         }
-    }
-
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
-            InputStreamReader reader = new InputStreamReader(resource.getInputStream());
-
-            // GsonBuilder를 사용하여 lenient 모드 설정
-            GsonBuilder gsonBuilder = new GsonBuilder();
-            gsonBuilder.setLenient();
-
-            var options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
-                    .build();
-
-            return FirebaseApp.initializeApp(options);
-        }
-        return FirebaseApp.getInstance();
     }
 }
