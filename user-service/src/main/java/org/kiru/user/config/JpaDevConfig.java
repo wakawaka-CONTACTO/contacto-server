5ee50662560abdf325fbcaf72c5d33ae8e884774
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-04-11 12:54:10 +0900

    Update JpaDevConfig.java

--- user-service/src/main/java/org/kiru/user/config/JpaDevConfig.java
+++ user-service/src/main/java/org/kiru/user/config/JpaDevConfig.java
@@ -3,6 +3,7 @@ package org.kiru.user.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @Configuration
 @Profile("dev")
