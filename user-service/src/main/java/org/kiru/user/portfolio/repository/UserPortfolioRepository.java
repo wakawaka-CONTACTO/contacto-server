ba1404857b578054fac4fe2d7feed13b6da7f65a
dlwhsk0 <hana.dev.314@gmail.com>
2025-04-18 14:19:30 +0900

    fix [#232] 매치/디테일에서 주는 이미지 개수 정합성 이슈

--- user-service/src/main/java/org/kiru/user/portfolio/repository/UserPortfolioRepository.java
+++ user-service/src/main/java/org/kiru/user/portfolio/repository/UserPortfolioRepository.java
@@ -12,7 +12,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPortfolioRepository extends JpaRepository<UserPortfolioImg, Long> {
-    @Query("SELECT upi FROM UserPortfolioImg upi WHERE upi.userId = :userId ORDER BY upi.sequence LIMIT 10")
+    @Query(
+        value = "SELECT * FROM user_portfolio_imgs WHERE user_id = :userId ORDER BY sequence LIMIT 10",
+        nativeQuery = true
+    )
     List<UserPortfolioImg> findAllByUserId(Long userId);
 
     void deleteAllByUserId(Long userId);
@@ -28,18 +31,37 @@ public interface UserPortfolioRepository extends JpaRepository<UserPortfolioImg,
     })
     List<UserPortfolioImg> findAllByUserIdInWithItemUrlMinSequence(List<Long> userIds);
 
-    @Query(value = """
-    SELECT 
-        MIN(upi.portfolio_id) AS portfolioId,
-        upi.user_id AS userId,
-        upi.username AS username,
-        STRING_AGG(upi.portfolio_image_url, ',' ORDER BY upi.sequence) AS portfolioImageUrl
-    FROM 
-        user_portfolio_imgs upi
-    WHERE 
-        upi.user_id IN (:userIds)
-    GROUP BY 
-        upi.user_id, upi.username
+//    @Query(value = """
+//    SELECT
+//        MIN(upi.portfolio_id) AS portfolioId,
+//        upi.user_id AS userId,
+//        upi.username AS username,
+//        STRING_AGG(upi.portfolio_image_url, ',' ORDER BY upi.sequence) AS portfolioImageUrl
+//    FROM
+//        user_portfolio_imgs upi
+//    WHERE
+//        upi.user_id IN (:userIds)
+//    GROUP BY
+//        upi.user_id, upi.username
+//    """, nativeQuery = true)
+@Query(value = """
+    WITH ranked_imgs AS (
+        SELECT
+            upi.*,
+            ROW_NUMBER() OVER (PARTITION BY upi.user_id ORDER BY upi.sequence) AS rn
+        FROM user_portfolio_imgs upi
+        WHERE upi.user_id IN (:userIds)
+    ),
+    limited_imgs AS (
+        SELECT * FROM ranked_imgs WHERE rn <= 10
+    )
+    SELECT
+        MIN(portfolio_id) AS portfolioId,
+        user_id AS userId,
+        username AS username,
+        STRING_AGG(portfolio_image_url, ',' ORDER BY sequence) AS portfolioImageUrl
+    FROM limited_imgs
+    GROUP BY user_id, username
     """, nativeQuery = true)
     @QueryHints(value = {
             @QueryHint(name = "org.hibernate.readOnly", value = "true"),
