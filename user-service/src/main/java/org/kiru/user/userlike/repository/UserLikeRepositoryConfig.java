d07e8982d6ba61031d3076915dde8139df386715
aristo <dkfud2121@naver.com>
2025-05-02 15:38:04 +0900

    refactor [#254] user like type 명시

--- user-service/src/main/java/org/kiru/user/userlike/repository/UserLikeRepositoryConfig.java
+++ user-service/src/main/java/org/kiru/user/userlike/repository/UserLikeRepositoryConfig.java
@@ -1,7 +1,9 @@
 package org.kiru.user.userlike.repository;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "org.kiru.user.userlike.repository")
 public class UserLikeRepositoryConfig {
 }
